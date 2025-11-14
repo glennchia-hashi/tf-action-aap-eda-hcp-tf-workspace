c4010b8a0f4d45367432883e4f89dd9afd2d0c41
glennchia-hashi <glenn.chia@hashicorp.com>
2025-11-14 11:14:51 +0800

    feat: aap tf configs

---
+++ 03-aap.tf
@@ -0,0 +1,37 @@
+locals {
+  organization_name = "Default"
+}
+
+data "aap_inventory" "this" {
+  name              = var.aap_inventory_name
+  organization_name = local.organization_name
+}
+
+resource "aap_host" "this" {
+  inventory_id = data.aap_inventory.this.id
+  name         = "tf-actions-demo-host"
+  description  = "Virtual machine with public IP ${aws_instance.tf_aap.public_ip}"
+
+  variables = jsonencode({
+    ansible_host = aws_instance.tf_aap.public_ip
+  })
+}
+
+data "aap_eda_eventstream" "this" {
+  name = var.aap_event_stream_name
+}
+
+action "aap_eda_eventstream_post" "create" {
+  config {
+    limit             = "demo"
+    template_type     = "job"
+    job_template_name = var.aap_job_template_name
+    organization_name = local.organization_name
+
+    event_stream_config = {
+      username = var.aap_event_stream_username
+      password = var.aap_event_stream_password
+      url      = data.aap_eda_eventstream.this.url
+    }
+  }
+}
